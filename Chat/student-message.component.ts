b6dee36d76219ba1c2300d5ac271642e3e313b2a
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-08-10 20:59:22 +0500

    Add files via upload

---
+++ Chat/student-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-message',
+  templateUrl: './student-message.component.html',
+  styleUrls: ['./student-message.component.css']
+})
+export class StudentMessageComponent {
+
+}
