97d340bf18b7d475faca07f2b210f6dde9c5c030
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-08-11 18:58:58 +0500

    Add files via upload

--- Chat/student-message.component.ts
+++ Chat/student-message.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-student-message',
   templateUrl: './student-message.component.html',
   styleUrls: ['./student-message.component.css']
 })
+
 export class StudentMessageComponent {
+username='username';
+messages=[];
+message='';
+searchControl= new FormControl('');
+ submit():void
+ {
+  
+ }
 
 }
