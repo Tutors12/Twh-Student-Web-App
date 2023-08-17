354b0e54eafd37e9f1b37aaf4f6b703aba761a6e
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-08-17 13:03:06 +0500

    Add files via upload

---
+++ Favorite-Tutor/favorite-tutor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-tutor',
+  templateUrl: './favorite-tutor.component.html',
+  styleUrls: ['./favorite-tutor.component.css']
+})
+export class FavoriteTutorComponent {
+
+}
