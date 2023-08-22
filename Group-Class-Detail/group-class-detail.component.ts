fba65c7f1d1bad8f8daa08e6a97566db0b4ac043
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-08-22 13:48:20 +0500

    Add files via upload

---
+++ Group-Class-Detail/group-class-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-class-detail',
+  templateUrl: './group-class-detail.component.html',
+  styleUrls: ['./group-class-detail.component.css']
+})
+export class GroupClassDetailComponent {
+
+}
