9c38adaf13b6a0542ae156eb94c836f93b5776f0
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-08-30 19:36:00 +0500

    Add files via upload

---
+++ Lesson Group Class/my-lesson.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-my-lesson',
+  templateUrl: './my-lesson.component.html',
+  styleUrls: ['./my-lesson.component.css'],
+
+})
+export class MyLessonComponent {
+  selected = 'option1';
+}
