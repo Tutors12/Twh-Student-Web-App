fac2096595dfe610747da4349701d544665565e0
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-09-15 19:29:33 +0500

    Add files via upload

---
+++ my-courses/mycoures.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycoures',
+  templateUrl: './mycoures.component.html',
+  styleUrls: ['./mycoures.component.css']
+})
+export class MycouresComponent {
+
+}
