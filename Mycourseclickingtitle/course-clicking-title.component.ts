903f5a6c9579b9d287679b70a7cf2d5282f7f79c
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-09-16 17:54:16 +0500

    Add files via upload

---
+++ Mycourseclickingtitle/course-clicking-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-clicking-title',
+  templateUrl: './course-clicking-title.component.html',
+  styleUrls: ['./course-clicking-title.component.css']
+})
+export class CourseClickingTitleComponent {
+
+}
