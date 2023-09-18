9972b42b6742c31b3d79b95cfbd75deb0ad473d9
Tutors Web Student App <141332639+Tutors12@users.noreply.github.com>
2023-09-18 13:46:09 +0500

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -9,12 +9,26 @@ import { FavoriteCourseMainComponent } from './favorite-course-main/favorite-cou
 import { FavoriteTutorComponent } from './favorite-tutor/favorite-tutor.component';
 import { MyLessonComponent } from './my-lesson/my-lesson.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CoursePaymentMethodComponent } from './course-payment-method/course-payment-method.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { TransactionPendingComponent } from './transaction-pending/transaction-pending.component';
+import { LessonInformationComponent } from './lesson-information/lesson-information.component';
+import { MycouresComponent } from './mycoures/mycoures.component';
+import { CourseClickingTitleComponent } from './course-clicking-title/course-clicking-title.component';
+import { MycourseEyeActionComponent } from './mycourse-eye-action/mycourse-eye-action.component';
 
 const routes: Routes = [
- 
-{path: '', component:LoginComponent},
+
+{path: 'mycourseclicktitle', component:CourseClickingTitleComponent},
+{path: 'Coursesye', component:MycourseEyeActionComponent},
+{path: 'transactionpending', component:TransactionPendingComponent},
+{path: 'mycourse', component:MycouresComponent},
+{path: 'transaction-history', component:TransactionHistoryComponent},
+{path: 'lesson-info', component:LessonInformationComponent},
+{path: 'coursepaymentmethod', component:CoursePaymentMethodComponent},
+{path: 'login', component:LoginComponent},
 {path: 'dashboard', component:DashboardComponent},
-{path: 'register', component:RegistrationComponent},
+{path: '', component:RegistrationComponent},
 {path: 'chatmessage', component:StudentMessageComponent},
 {path: 'favoritegroupclass', component:FavoriteGroupClassComponent},
 {path: 'favoritecoursemain', component:FavoriteCourseMainComponent},
